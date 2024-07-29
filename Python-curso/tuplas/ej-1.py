90534bc8b3718d036b89ba7a20eb325ca8f3417d
LeonardoMendezR <2108727@ucc.edu.ar>
2024-07-29 20:09:39 -0300

    Ejercicios tupla

---
+++ Python-curso/tuplas/ej-1.py
@@ -0,0 +1,4 @@
+#Crea una tupla con los elementos "manzana", "banana" y "cereza".
+
+tupla=("manzana","banana", "cereza")
+print(tupla)
---
+++ Python-curso/tuplas/ej-2.py
@@ -0,0 +1,9 @@
+
+def concat(tupla_1, tupla_2):
+    tupla_concatenada=tupla_1+tupla_2
+    return tupla_concatenada
+
+tupla_1=(1,2,3,4,5)
+tupla_2=(6,7,8,9,10)
+tupla_concatenada=concat(tupla_1,tupla_2)
+print("concatenacion de tuplas: ", tupla_concatenada)
